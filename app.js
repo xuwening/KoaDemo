55556c4f699b0b4cfde8fcaeadcc2fa2e5af303f
hongjingjun <hongjingjun@leadeon.cn>
2017-09-04 09:14:30 +0800

    添加示例

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const Koa = require('koa');
 const app = new Koa();
 
-app.use(async ctx => {
-  ctx.body = 'Hello World';
-});
+var router = require('koa-route'); 
+const koaBody = require('koa-body')
+
+
+// var route = router.post('/post', async function(ctx,next){  
+//   console.log(ctx.request.body)
+//   ctx.body = "you post data:"+JSON.stringify({id:id});  
+//  });
+
+const main = ctx => {
+  console.log(ctx.request.body)
+  ctx.body = 'haahdhd'
+}
+
+app.use(koaBody())
+app.use(router.post('/post', main))
+
+// app.use(async ctx => {
+//   console.log(ctx.request.body)
+//   ctx.body = 'Hello World';
+// });
 
 app.listen(3000);
 
